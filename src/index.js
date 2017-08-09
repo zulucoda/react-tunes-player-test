47a80e2e5c9ca67e567b361a6bb5633e85fa9be6
Muzikayise Flynn Buthelezi aka zuluCoda <muzi@mfbproject.co.za>
2017-08-09 18:56:23 +0200

    add action creators

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import AppContainer from "./app-container";
 import { Provider } from "react-redux";
 import store from "./store";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AppContainer />
   </Provider>,
   document.getElementById("root")
 );
