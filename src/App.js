8d4f22997b7fd429ce7fd71a7fe9504976d450f7
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2019-08-20 21:18:37 +0200

    update test react-tunes-player with v4.0.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ReactTunesPlayerContainer } from 'react-tunes-player';
+import ReactTunesPlayer from 'react-tunes-player';
 
 class App extends Component {
   render() {
@@ -15,7 +15,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <ReactTunesPlayerContainer
+        <ReactTunesPlayer
           tunes={[
             {
               tune: "/assets/audio/the_lego_tune.ogg",
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import AppContainer from "./app-container";
-import { Provider } from "react-redux";
-import store from "./store";
+import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <AppContainer />
-  </Provider>,
+    <App />,
   document.getElementById("root")
 );
 registerServiceWorker();
